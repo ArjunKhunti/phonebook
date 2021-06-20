26d4f20585053feb0bfc4b8e5d7926f6ae2787cd
ArjunKhunti <arjunkhunti@yahoo.com>
2021-06-20 17:39:36 +0530

    added components to search contact and update contact

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import user from "../images/user.jpg";
 
 const ContactCard = (props) => {
-    const {id, name, email} = props.contact;
-    return (
-        <div className="item">
-            <img className="ui avatar image" src={user} alt="user" />
-            <div className="content">
-                <div className="header">{name}</div>
-                <div>{email}</div>
-            </div>
-            <i className="trash alternate outline icon" style={{color: "red", marginTop: "7px"}} onClick={() => props.clickHandler(id)}></i>
+  const { id, name, email } = props.contact;
+  return (
+    <div className="item">
+      <img className="ui avatar image" src={user} alt="user" />
+      <Link
+        to={{ pathname: `/contact/${id}`, state: { contact: props.contact } }}
+      >
+        <div className="content">
+          <div className="header">{name}</div>
+          <div>{email}</div>
         </div>
-    );
-}
+      </Link>
+      <div>
+        <Link to={{ pathname: `/edit`, state: { contact: props.contact } }}>
+          <i
+            className="edit alternate outline icon"
+            style={{ color: "blue", marginTop: "7px", marginLeft: "10px" }}
+          ></i>
+        </Link>
+        <i
+          className="trash alternate outline icon"
+          style={{ color: "red", marginTop: "7px", marginLeft: "10px" }}
+          onClick={() => props.clickHandler(id)}
+        ></i>
+      </div>
+    </div>
+  );
+};
 
 export default ContactCard;
